f5935a804d95da8641faa7166ffcb2ea9ce8bbe4
Clément Moulia <clement.moulia@consulting-for.accor.com>
2019-11-06 12:33:02 +0100

    Minification OK
    MInification fonctionne
    Reste config à intégrer
    Et minification css

--- hooks/hooks.js
+++ hooks/hooks.js
@@ -1,16 +1,29 @@
 'use strict'
-var path = require('path')
-var fs = require('fs')
-var {crush} = require('html-crush')
-var {comb} = require('email-comb')
+const path = require('path')
+const fs = require('fs')
+const util = require('util')
+const {crush} = require('html-crush')
+const {comb} = require('email-comb')
+
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
+var configName = 'abe-emailMinify'
 
 var hooks = {
   afterPublish: function(result, postPath, abe){
-    console.log(result, postPath, abe)
-    if (abe.config["abe-emailMinify"].active){
+    if (true || abe.config[configName] && abe.config[configName].active){
       const documentPath = path.join(abe.config.root, abe.config.publish.url)
-			const fileName = path.basename(result.abe_meta.publish.abeUrl)
-			const packageDate = Math.floor(new Date())
+      const baseName = path.basename(result.abe_meta.publish.abeUrl)
+      const extName = path.extname(result.abe_meta.publish.abeUrl)
+      const filePath = path.join(documentPath, baseName)
+      
+      readFile(filePath, 'utf8').then((data) => {
+          var crushed = crush(data)
+          const destFileName = path.basename(baseName, extName) + '.min' + extName
+          const destFilePath = path.join(path.dirname(filePath), destFileName)
+          writeFile(destFilePath, crushed.result).catch(err => console.log(err))
+      })
     }
   }
 }
